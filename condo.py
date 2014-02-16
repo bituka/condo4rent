fee862579eb847eacebc69ca40624fb6bbedbc8d
goryo <goryo.webdev@gmail.com>
2014-02-16 22:16:23 +0800

    update

--- condo.py
+++ condo.py
@@ -1,25 +1,24 @@
 import webapp2
 import jinja2
 import os
-import cgi
-import datetime
-import urllib
-import sys
+# import cgi
+# import datetime
+# import urllib
+# import sys
 
 from google.appengine.ext import db
-from google.appengine.api import images
-from google.appengine.api import users
-from google.appengine.api import mail
+# from google.appengine.api import users
+
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 #models 
 class UtilitiesComputation(db.Model):
-    checkin_elec = db.IntegerProperty()
-    checkout_elec = db.IntegerProperty()
-    checkin_water = db.IntegerProperty()
-    checkout_water = db.IntegerProperty()
+    checkin_elec = db.FloatProperty()
+    checkout_elec = db.FloatProperty()
+    checkin_water = db.FloatProperty()
+    checkout_water = db.FloatProperty()
     tenant_name = db.StringProperty()
     date = db.DateTimeProperty(auto_now_add=True)
     
@@ -48,10 +47,10 @@ class UtilitiesComputationPage(webapp2.RequestHandler):
     def post(self):   
         utilitiescomputation = UtilitiesComputation()
         utilitiescomputation.tenant_name = self.request.get('tenant_name')
-        utilitiescomputation.checkin_elec = int(self.request.get('checkin_elec'))
-        utilitiescomputation.checkin_water = int(self.request.get('checkin_water'))
-        utilitiescomputation.checkout_elec = int(self.request.get('checkout_elec'))
-        utilitiescomputation.checkout_water = int(self.request.get('checkout_water'))
+        utilitiescomputation.checkin_elec = float(self.request.get('checkin_elec'))
+        utilitiescomputation.checkin_water = float(self.request.get('checkin_water'))
+        utilitiescomputation.checkout_elec = float(self.request.get('checkout_elec'))
+        utilitiescomputation.checkout_water = float(self.request.get('checkout_water'))
         
         utilitiescomputation.put()
     #    item.key().id()
@@ -82,7 +81,7 @@ class LatestUtilityEntry(webapp2.RequestHandler):
         elec_charge = elec_consume * elec_cpu 
         water_charge = water_cpu * water_consume
 
-        total_utilities_charge = elec_charge + water_charge
+        total_utilities_charge = round(elec_charge + water_charge, 2)
         template_values = {
             'elec_consume' : elec_consume,
             'water_consume' : water_consume,
@@ -97,93 +96,12 @@ class LatestUtilityEntry(webapp2.RequestHandler):
         template = jinja_environment.get_template('company/latestutilityentry_added.html')
         self.response.out.write(template.render(template_values)) 
 
-class EmailUtilitiesDraft(webapp2.RequestHandler):
-#    def get(self):
-
-#         user = users.get_current_user()
-
-#         if (user and user.nickname() == 'makaticondo4rent'):
-           
-#            checkin_elec = self.request.get('checkin_elec')
-#            checkout_elec = self.request.get('checkout_elec')
-#            checkin_water = self.request.get('checkin_elec')
-#            checkout_water = self.request.get('checkout_water')
-#            elec_consume  = self.request.get('elec_consume')
-#            water_consume = self.request.get('water_consume')
-#            elec_cpu = self.request.get('elec_cpu')
-#            water_cpu = self.request.get('water_cpu')
-
-#            template_values = {
-#             'elec_consume' : elec_consume,
-#             'water_consume' : water_consume,
-#             'water_charge' : water_charge,  
-#             'elec_charge' : elec_charge,   
-#             'elec_cpu' : elec_cpu,
-#             'water_cpu' : water_cpu,
-#             'total_utilities_charge' : total_utilities_charge
-#             }
-#         else:
-#             self.redirect(users.create_login_url(self.request.uri))
-
-    def post(self):
-        if (user and user.nickname() == 'makaticondo4rent'):
-
-           checkin_elec = self.request.get('checkin_elec')
-           checkout_elec = self.request.get('checkout_elec')
-           checkin_water = self.request.get('checkin_elec')
-           checkout_water = self.request.get('checkout_water')
-           elec_consume  = self.request.get('elec_consume')
-           water_consume = self.request.get('water_consume')
-           elec_cpu = self.request.get('elec_cpu')
-           water_cpu = self.request.get('water_cpu')
-
-
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
-
-#send utilities computation to tenant
-class SendUtilitiesTenant(webapp2.RequestHandler):
-
-    def post(self):
-
-        user = users.get_current_user()
-
-        if (user and user.nickname() == 'makaticondo4rent'):
-
-            checkin_elec = self.request.get('checkin_elec')
-            checkout_elec = self.request.get('checkout_elec')
-            checkin_water = self.request.get('checkin_elec')
-            checkout_water = self.request.get('checkout_water')
-            elec_consume  = self.request.get('elec_consume')
-            water_consume = self.request.get('water_consume')
-            elec_cpu = self.request.get('elec_cpu')
-            water_cpu = self.request.get('water_cpu')
-            to_email = self.request.get('to_email')
-
-            message = mail.EmailMessage()
-            message.sender = user.email()
-            message.subject = "Test Mail"
-            message.to = to_email
-            message.html = """
-                <html>
-                <header><title></title></header>
-                <body> %s   </body>
-                </html>
-                """ % checkin_elec
-            message.send() 
-        
-        else:
-            
-            self.redirect(users.create_login_url(self.request.uri))
-
         
 
-application = webapp2.WSGIApplication([
+app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/latestutilityentry', LatestUtilityEntry),
     ('/utilitiescomputation', UtilitiesComputationPage),
-    ('/sendutilitiestenant', SendUtilitiesTenant),
-    ('/emailutilitiesdraft', EmailUtilitiesDraft),
     
 ], debug=True)
 
