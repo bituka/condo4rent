f2d92bbb795dcfbee0c2dc7dedd652c60cabf43e
goryo <goryo.webdev@gmail.com>
2014-02-18 17:02:49 +0800

    sup

--- condo.py
+++ condo.py
@@ -7,7 +7,7 @@ import os
 # import sys
 
 from google.appengine.ext import db
-# from google.appengine.api import users
+from google.appengine.api import users
 
 
 jinja_environment = jinja2.Environment(
@@ -37,72 +37,87 @@ class MainPage(webapp2.RequestHandler):
         
 class UtilitiesComputationPage(webapp2.RequestHandler):
 
-    def get(self):
-        template_values = {
+    user = users.get_current_user()
+
+    if (user and user.nickname() == 'makaticondo4rent'):
+
+        def get(self):
+            template_values = {
+                
+            }
             
-        }
-        
-        template = jinja_environment.get_template('company/utilities_main.html')
-        self.response.out.write(template.render(template_values))
+            template = jinja_environment.get_template('company/utilities_main.html')
+            self.response.out.write(template.render(template_values))
 
-    def post(self):   
-        utilitiescomputation = UtilitiesComputation()
-        utilitiescomputation.tenant_name = self.request.get('tenant_name')
-        utilitiescomputation.checkin_elec = float(self.request.get('checkin_elec'))
-        utilitiescomputation.checkin_water = float(self.request.get('checkin_water'))
-        utilitiescomputation.checkout_elec = float(self.request.get('checkout_elec'))
-        utilitiescomputation.checkout_water = float(self.request.get('checkout_water'))
-        utilitiescomputation.deposit = float(self.request.get('deposit'))
-        
-        utilitiescomputation.put()
-    #    item.key().id()
+        def post(self):   
+            utilitiescomputation = UtilitiesComputation()
+            utilitiescomputation.tenant_name = self.request.get('tenant_name')
+            utilitiescomputation.checkin_elec = float(self.request.get('checkin_elec'))
+            utilitiescomputation.checkin_water = float(self.request.get('checkin_water'))
+            utilitiescomputation.checkout_elec = float(self.request.get('checkout_elec'))
+            utilitiescomputation.checkout_water = float(self.request.get('checkout_water'))
+            utilitiescomputation.deposit = float(self.request.get('deposit'))
+            
+            utilitiescomputation.put()
+        #    item.key().id()
 
-    #    redirectUrl = "/view/key/%s/" % item.key()
-    #    self.redirect(redirectUrl)
-    #    test = utilitiescomputation.key().id()
-    #    print test 
+        #    redirectUrl = "/view/key/%s/" % item.key()
+        #    self.redirect(redirectUrl)
+        #    test = utilitiescomputation.key().id()
+        #    print test 
 
-        redirectUrl = "/latestutilityentry?id=%s" % utilitiescomputation.key()
-    #    print redirectUrl        
+            redirectUrl = "/latestutilityentry?id=%s" % utilitiescomputation.key()
+        #    print redirectUrl        
 
-        self.redirect(redirectUrl)
-    #    self.redirect('/latestutilityentry', utilitiescomputation.key().id())            
+            self.redirect(redirectUrl)
+        #    self.redirect('/latestutilityentry', utilitiescomputation.key().id())   
+
+    else:    
+        self.redirect(users.create_login_url(self.request.uri))                 
 
 #display latest added computation after adding entry
 class LatestUtilityEntry(webapp2.RequestHandler):
-    def get(self):   
-        #cpu means charge per unit
-        elec_cpu = 12
-        water_cpu = 18
 
-        utilitiescomputation = db.get(self.request.get('id'))
-        
-        elec_consume = utilitiescomputation.checkout_elec - utilitiescomputation.checkin_elec
-        water_consume = utilitiescomputation.checkout_water - utilitiescomputation.checkin_water
+    user = users.get_current_user()
 
-        elec_charge = elec_consume * elec_cpu 
-        water_charge = water_cpu * water_consume
+    if (user and user.nickname() == 'makaticondo4rent'):
 
-        total_utilities_charge = round(elec_charge + water_charge, 2)
-        refund = utilitiescomputation.deposit - total_utilities_charge
+        def get(self):   
+            #cpu means charge per unit
+            elec_cpu = 12
+            water_cpu = 18
 
-        template_values = {
-            'elec_consume' : elec_consume,
-            'water_consume' : water_consume,
-            'utilitiescomputation': utilitiescomputation,
-            'water_charge' : water_charge,  
-            'elec_charge' : elec_charge,   
-            'elec_cpu' : elec_cpu,
-            'water_cpu' : water_cpu,
-            'total_utilities_charge' : total_utilities_charge,
-            'refund' : refund
-        }
-        
-        template = jinja_environment.get_template('company/latestutilityentry_added.html')
-        self.response.out.write(template.render(template_values)) 
+            utilitiescomputation = db.get(self.request.get('id'))
+            
+            elec_consume = utilitiescomputation.checkout_elec - utilitiescomputation.checkin_elec
+            water_consume = utilitiescomputation.checkout_water - utilitiescomputation.checkin_water
+
+            elec_charge = elec_consume * elec_cpu 
+            water_charge = water_cpu * water_consume
+
+            total_utilities_charge = round(elec_charge + water_charge, 2)
+            refund = utilitiescomputation.deposit - total_utilities_charge
+
+            template_values = {
+                'elec_consume' : elec_consume,
+                'water_consume' : water_consume,
+                'utilitiescomputation': utilitiescomputation,
+                'water_charge' : water_charge,  
+                'elec_charge' : elec_charge,   
+                'elec_cpu' : elec_cpu,
+                'water_cpu' : water_cpu,
+                'total_utilities_charge' : total_utilities_charge,
+                'refund' : refund
+            }
+            
+            template = jinja_environment.get_template('company/latestutilityentry_added.html')
+            self.response.out.write(template.render(template_values)) 
+    else:
+        self.redirect(users.create_login_url(self.request.uri))    
 
 #display balance for ate dhanna
 class BalancePage(webapp2.RequestHandler):
+
     def get(self):   
         
         template_values = {
